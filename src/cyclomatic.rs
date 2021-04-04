062220c9143e5c7817ac17ff356d19d827b7a6b1
Satyarth Agrahari <satylogin@gmail.com>
2021-04-04 14:48:31 +0530

    transposing ast to tradition graph

--- src/cyclomatic.rs
+++ src/cyclomatic.rs
@@ -9,142 +9,163 @@ use syn;
 
 pub fn calculate_complexity(file_path: String) -> i64 {
     let ast = get_ast(file_path).ok().unwrap();
-    let ast = ASTGraph::new(ast);
-    dbg!(&ast);
-    ast.edges - ast.nodes + 2 * ast.connected_components
+    let _ast = ASTGraph::new(ast);
+    0
 }
 
 #[derive(Debug, PartialEq)]
-pub struct ASTGraph {
-    pub connected_components: i64,
-    pub nodes: i64,
-    pub edges: i64,
-    visited: HashSet<u64>,
+struct ASTGraph {
+    nodes: HashSet<u64>,
+    edges: Vec<(u64, u64)>,
+}
+
+fn get_node<T: Hash>(ast: T) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    ast.hash(&mut hasher);
+
+    hasher.finish()
 }
 
 impl ASTGraph {
     pub fn new(ast: syn::File) -> ASTGraph {
         let mut ast_graph = ASTGraph {
-            connected_components: 0,
-            nodes: 0,
-            edges: 0,
-            visited: HashSet::new(),
+            nodes: HashSet::new(),
+            edges: vec![],
         };
-        ast_graph.parse_file(ast);
+        ast_graph.parse_file(ast, 0);
+
         ast_graph
     }
 
-    fn checked_update<T: Hash>(&mut self, ast: T) -> bool {
-        self.edges += 1;
-        let mut hasher = DefaultHasher::new();
-        ast.hash(&mut hasher);
-        let addr = hasher.finish();
+    fn checked_update(&mut self, node: u64, parent: u64) -> bool {
+        self.edges.push((parent, node));
 
-        if self.visited.contains(&addr) {
+        if self.nodes.contains(&node) {
             false
         } else {
-            self.visited.insert(addr);
-            self.nodes += 1;
+            self.nodes.insert(node);
             true
         }
     }
 
-    fn parse_file(&mut self, ast: syn::File) {
-        for item in ast.items {
-            self.connected_components += 1;
-            self.parse_item(item);
+    fn parse_file(&mut self, ast: syn::File, parent: u64) {
+        let node = get_node(&ast);
+        if self.checked_update(node, parent) {
+            for item in ast.items {
+                self.parse_item(item, node);
+            }
         }
     }
 
-    fn parse_item(&mut self, ast: syn::Item) {
-        if self.checked_update(&ast) {
+    fn parse_item(&mut self, ast: syn::Item, parent: u64) {
+        let node = get_node(&ast);
+        if self.checked_update(node, parent) {
             match ast {
-                syn::Item::ExternCrate(ast) => self.parse_item_extern_crate(ast),
-                syn::Item::Use(ast) => self.parse_item_use(ast),
-                syn::Item::Static(ast) => self.parse_item_static(ast),
-                syn::Item::Const(ast) => self.parse_item_const(ast),
-                syn::Item::Fn(ast) => self.parse_item_fn(ast),
-                syn::Item::Mod(ast) => self.parse_item_mod(ast),
-                syn::Item::ForeignMod(ast) => self.parse_item_foreign_mod(ast),
-                syn::Item::Type(ast) => self.parse_item_type(ast),
-                syn::Item::Struct(ast) => self.parse_item_struct(ast),
-                syn::Item::Enum(ast) => self.parse_item_enum(ast),
-                syn::Item::Union(ast) => self.parse_item_union(ast),
-                syn::Item::Trait(ast) => self.parse_item_trait(ast),
-                syn::Item::TraitAlias(ast) => self.parse_item_trait_alias(ast),
-                syn::Item::Impl(ast) => self.parse_item_impl(ast),
-                syn::Item::Macro(ast) => self.parse_item_macro(ast),
-                syn::Item::Macro2(ast) => self.parse_item_macro2(ast),
+                syn::Item::ExternCrate(ast) => self.parse_item_extern_crate(ast, node),
+                syn::Item::Use(ast) => self.parse_item_use(ast, node),
+                syn::Item::Static(ast) => self.parse_item_static(ast, node),
+                syn::Item::Const(ast) => self.parse_item_const(ast, node),
+                syn::Item::Fn(ast) => self.parse_item_fn(ast, node),
+                syn::Item::Mod(ast) => self.parse_item_mod(ast, node),
+                syn::Item::ForeignMod(ast) => self.parse_item_foreign_mod(ast, node),
+                syn::Item::Type(ast) => self.parse_item_type(ast, node),
+                syn::Item::Struct(ast) => self.parse_item_struct(ast, node),
+                syn::Item::Enum(ast) => self.parse_item_enum(ast, node),
+                syn::Item::Union(ast) => self.parse_item_union(ast, node),
+                syn::Item::Trait(ast) => self.parse_item_trait(ast, node),
+                syn::Item::TraitAlias(ast) => self.parse_item_trait_alias(ast, node),
+                syn::Item::Impl(ast) => self.parse_item_impl(ast, node),
+                syn::Item::Macro(ast) => self.parse_item_macro(ast, node),
+                syn::Item::Macro2(ast) => self.parse_item_macro2(ast, node),
                 syn::Item::Verbatim(_) => { /*skipping since i cann't access*/ }
                 syn::Item::__TestExhaustive(_) => { /*skipping private things*/ }
             }
         }
     }
 
-    fn parse_item_extern_crate(&mut self, ast: syn::ItemExternCrate) {
-        if self.checked_update(&ast) {
-            if ast.rename.is_some() {
-                self.nodes += 2;
-                self.edges += 2;
+    // TODO: add sub checks
+    fn parse_item_extern_crate(&mut self, ast: syn::ItemExternCrate, parent: u64) {
+        let node = get_node(&ast);
+        self.checked_update(node, parent);
+    }
+
+    fn parse_item_use(&mut self, ast: syn::ItemUse, parent: u64) {
+        let node = get_node(&ast);
+        if self.checked_update(node, parent) {
+            self.parse_use_tree(ast.tree, node);
+        }
+    }
+
+    fn parse_item_static(&mut self, ast: syn::ItemStatic, parent: u64) {
+        let node = get_node(&ast);
+        if self.checked_update(node, parent) {
+            self.parse_type(*ast.ty, node);
+            self.parse_expr(*ast.expr, node);
+        }
+    }
+
+    fn parse_item_const(&mut self, ast: syn::ItemConst, parent: u64) {
+        let node = get_node(&ast);
+        if self.checked_update(node, parent) {
+            self.parse_type(*ast.ty, node);
+            self.parse_expr(*ast.expr, node);
+        }
+    }
+
+    fn parse_item_fn(&mut self, ast: syn::ItemFn, parent: u64) {
+        let node = get_node(&ast);
+        if self.checked_update(node, parent) {
+            self.parse_block(*ast.block, node);
+        }
+    }
+
+    fn parse_item_mod(&mut self, ast: syn::ItemMod, parent: u64) {
+        let node = get_node(&ast);
+        if self.checked_update(node, parent) {
+            if let Some((_, items)) = ast.content {
+                for item in items {
+                    self.parse_item(item, node);
+                }
             }
         }
     }
 
-    fn parse_item_use(&mut self, ast: syn::ItemUse) {
-        if self.checked_update(&ast) {
-            self.parse_use_tree(ast.tree);
+    fn parse_item_foreign_mod(&mut self, ast: syn::ItemForeignMod, parent: u64) {
+        let node = get_node(&ast);
+        if self.checked_update(node, parent) {
+            for item in ast.items {
+                self.parse_foreign_item(item, node);
+            }
         }
     }
 
-    fn parse_item_static(&mut self, ast: syn::ItemStatic) {
-        if self.checked_update(&ast) {
-            self.parse_type(*ast.ty);
-            self.parse_expr(*ast.expr);
-        }
-    }
+    fn parse_item_type(&mut self, _: syn::ItemType, _: u64) {}
 
-    fn parse_item_const(&mut self, ast: syn::ItemConst) {
-        if self.checked_update(&ast) {
-            self.parse_type(*ast.ty);
-            self.parse_expr(*ast.expr);
-        }
-    }
+    fn parse_item_struct(&mut self, _: syn::ItemStruct, _: u64) {}
 
-    fn parse_item_fn(&mut self, ast: syn::ItemFn) {
-        if self.checked_update(&ast) {
-            self.parse_block(*ast.block);
-        }
-    }
+    fn parse_item_enum(&mut self, _: syn::ItemEnum, _: u64) {}
 
-    fn parse_item_mod(&mut self, _: syn::ItemMod) {}
+    fn parse_item_union(&mut self, _: syn::ItemUnion, _: u64) {}
 
-    fn parse_item_foreign_mod(&mut self, _: syn::ItemForeignMod) {}
+    fn parse_item_trait(&mut self, _: syn::ItemTrait, _: u64) {}
 
-    fn parse_item_type(&mut self, _: syn::ItemType) {}
+    fn parse_item_trait_alias(&mut self, _: syn::ItemTraitAlias, _: u64) {}
 
-    fn parse_item_struct(&mut self, _: syn::ItemStruct) {}
+    fn parse_item_impl(&mut self, _: syn::ItemImpl, _: u64) {}
 
-    fn parse_item_enum(&mut self, _: syn::ItemEnum) {}
+    fn parse_item_macro(&mut self, _: syn::ItemMacro, _: u64) {}
 
-    fn parse_item_union(&mut self, _: syn::ItemUnion) {}
+    fn parse_item_macro2(&mut self, _: syn::ItemMacro2, _: u64) {}
 
-    fn parse_item_trait(&mut self, _: syn::ItemTrait) {}
+    fn parse_use_tree(&mut self, _: syn::UseTree, _: u64) {}
 
-    fn parse_item_trait_alias(&mut self, _: syn::ItemTraitAlias) {}
+    fn parse_type(&mut self, _: syn::Type, _: u64) {}
 
-    fn parse_item_impl(&mut self, _: syn::ItemImpl) {}
+    fn parse_expr(&mut self, _: syn::Expr, _: u64) {}
 
-    fn parse_item_macro(&mut self, _: syn::ItemMacro) {}
+    fn parse_block(&mut self, _: syn::Block, _: u64) {}
 
-    fn parse_item_macro2(&mut self, _: syn::ItemMacro2) {}
-
-    fn parse_use_tree(&mut self, _: syn::UseTree) {}
-
-    fn parse_type(&mut self, _: syn::Type) {}
-
-    fn parse_expr(&mut self, _: syn::Expr) {}
-
-    fn parse_block(&mut self, _: syn::Block) {}
+    fn parse_foreign_item(&mut self, _: syn::ForeignItem, _: u64) {}
 }
 
 type ParseResult<T> = Result<T, Box<dyn Error + 'static>>;
