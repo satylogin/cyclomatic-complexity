98102173710e2da0b0f1901cf42878b77f78b116
Satyarth Agrahari <satylogin@gmail.com>
2021-05-09 08:08:10 +0530

    adding expr block parsing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,12 +8,10 @@ fn main() {
     if config.is_err() {
         println!("{}", config.err().unwrap().message);
         return;
-    } else if 2 % 2 == 0 {
-        // have something here
-    } else if 2 % 3 == 0 {
-        // something
     } else {
-        // something
+        if 1 % 2 == 0 {
+        } else if 2 % 2 == 0 {
+        }
     }
     let config: Config = config.ok().unwrap();
     rust_parser::display_complexity(config.file);
